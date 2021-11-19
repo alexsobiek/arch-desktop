c5de0594b7ade10aebb06ee5fddf991e881f3697
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-11-19 01:34:43 -0600

    Created schema overrides

--- budgie.sh
+++ budgie.sh
@@ -23,11 +23,6 @@ git fetch
 git checkout ocean
 cd
 mv /tmp/Juno-ocean /usr/share/themes
-sudo -u lightdm gsettings set org.gnome.desktop.interface gtk-theme Juno-ocean
-sudo -u lightdm gsettings set org.gnome.desktop.wm.preferences theme Juno-ocean
-sudo -u lightdm gsettings set com.solus-project.budgie-panel builtin-theme false
-sudo -u lightdm gsettings set com.solus-project.budgie-wm button-layout close,minimize,maximize:appmenu
-sudo -u lightdm gsettings set org.gnome.desktop.wm.preferences button-layout close,minimize,maximize:appmenu
 
 # Icon Theme
 cd /tmp
@@ -35,23 +30,27 @@ curl -LO https://github.com/dracula/gtk/files/5214870/Dracula.zip
 bsdtar xf Dracula.zip
 mv Dracula /usr/share/icons
 curl -O https://upload.wikimedia.org/wikipedia/commons/a/a5/Archlinux-icon-crystal-64.svg --output-dir /usr/share/icons/
-sudo -u lightdm gsettings set org.gnome.desktop.interface icon-theme Dracula
 
 # Desktop Background
 cd /usr/share/backgrounds
 curl https://www.teahub.io/photos/full/123-1238135_arch-linux-wallpapers-colorfull.jpg -o background1.jpg
 curl https://i.imgur.com/XgCwWhw.png -o background2.png
-sudo -u lightdm gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/background1.jpg
 
 # Budgie Panel
 cd /tmp
 curl -O https://raw.githubusercontent.com/alexsobiek/arch-desktop/main/budgie/budgie-dconf
 sudo -u lightdm donf load /com/solus-project < budgie-donf
 
+# Schema Overrides
+curl -O https://raw.githubusercontent.com/alexsobiek/arch-desktop/main/budgie/budgie.gschema.overide --output-dir /usr/share/glib-2.0/schemas/
+glib-compile-schemas /usr/share/glib-2.0/schemas
+
 # Programs
 pacman -S gnome-control-center gnome-software gnome-software-packagekit-plugin --noconfirm
 pacman -S tilix htop neofetch nautilus --noconfirm
 pacman -S firefox-developer-edition --noconfirm
+
+
 
 # Done
 systemctl reboot
