174a3568a6a5642caadbb8ff9e954ad15d0294c0
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-11-19 03:03:29 -0600

    Use of panel.ini override

--- budgie.sh
+++ budgie.sh
@@ -39,7 +39,7 @@ curl https://i.imgur.com/XgCwWhw.png -o background2.png
 # Schema Overrides
 curl -O https://raw.githubusercontent.com/alexsobiek/arch-desktop/main/budgie/budgie.gschema.override --output-dir /usr/share/glib-2.0/schemas/
 mkdir -p /usr/share/budgie-desktop/layouts
-curl -O https://raw.githubusercontent.com/alexsobiek/arch-desktop/main/budgie/budgie-panel.layout --output-dir /usr/share/budgie-desktop/layouts
+curl -O https://raw.githubusercontent.com/alexsobiek/arch-desktop/main/budgie/panel.ini --output-dir /usr/share/budgie-desktop/
 glib-compile-schemas /usr/share/glib-2.0/schemas
 
 # Programs
