b078d2d73d62188ee3373cb996aada9e0eaa0e3e
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-11-19 00:45:52 -0600

    Run gsettings as lightdm user

--- budgie.sh
+++ budgie.sh
@@ -3,10 +3,10 @@
 # Needs to be run as root or with sudo
 
 # Utility
-pacman -S git --noconfirm
+pacman -S sudo git --noconfirm
 
 # Budgie
-pacman -S budgie-desktop budgie-screensaver --noconfirm
+pacman -S budgie-desktop budgie-screensaver --noconfirmsyus
 
 # LightDM
 pacman -S xorg-server --noconfirm
@@ -22,11 +22,11 @@ git fetch
 git checkout ocean
 cd
 mv /tmp/Juno-ocean /usr/share/themes
-gsettings set org.gnome.desktop.interface gtk-theme Juno-ocean
-gsettings set org.gnome.desktop.wm.preferences theme Juno-ocean
-gsettings set com.solus-project.budgie-panel builtin-theme false
-gsettings set com.solus-project.budgie-wm button-layout close,minimize,maximize:appmenu
-gsettings set org.gnome.desktop.wm.preferences button-layout close,minimize,maximize:appmenu
+sudo -u lightdm gsettings set org.gnome.desktop.interface gtk-theme Juno-ocean
+sudo -u lightdm gsettings set org.gnome.desktop.wm.preferences theme Juno-ocean
+sudo -u lightdm gsettings set com.solus-project.budgie-panel builtin-theme false
+sudo -u lightdm gsettings set com.solus-project.budgie-wm button-layout close,minimize,maximize:appmenu
+sudo -u lightdm gsettings set org.gnome.desktop.wm.preferences button-layout close,minimize,maximize:appmenu
 
 # Icon Theme
 cd /tmp
@@ -34,18 +34,18 @@ curl -LO https://github.com/dracula/gtk/files/5214870/Dracula.zip
 bsdtar xf Dracula.zip
 mv Dracula /usr/share/icons
 curl -O https://upload.wikimedia.org/wikipedia/commons/a/a5/Archlinux-icon-crystal-64.svg --output-dir /usr/share/icons/
-gsettings set org.gnome.desktop.interface icon-theme Dracula
+sudo -u lightdm gsettings set org.gnome.desktop.interface icon-theme Dracula
 
 # Desktop Background
 cd /usr/share/backgrounds
 curl https://www.teahub.io/photos/full/123-1238135_arch-linux-wallpapers-colorfull.jpg -o background1.jpg
 curl https://i.imgur.com/XgCwWhw.png -o background2.png
-gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/background1.jpg
+sudo -u lightdm gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/background1.jpg
 
 # Budgie Panel
 cd /tmp
 curl -O https://raw.githubusercontent.com/alexsobiek/arch-desktop/main/budgie/budgie-dconf
-donf load /com/solus-project < budgie-donf
+sudo -u lightdm donf load /com/solus-project < budgie-donf
 
 # Programs
 pacman -S gnome-control-center gnome-software gnome-software-packagekit-plugin --noconfirm
